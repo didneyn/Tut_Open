63e57022ee5d03bf6b0d6b3d241736a441ed6452
didneyn <didney.nieto@mercadolibre.com.co>
2022-05-31 18:40:51 -0500

    add session 5

---
+++ Spring/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
@@ -0,0 +1,25 @@
+package com.example.obrestdatajpa;
+
+import com.example.obrestdatajpa.entities.Book;
+import com.example.obrestdatajpa.repositories.BookRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class ObRestDatajpaApplication {
+
+    public static void main(String[] args) {
+      ApplicationContext context= SpringApplication.run(ObRestDatajpaApplication.class, args);
+      BookRepository repository= context.getBean(BookRepository.class);
+
+      Book book = new Book(null, "strellas", "Jose Caro", 160, 200000.0, LocalDate.now(),true);
+      Book book2 = new Book(null, "Tranquilidad", "Pedro perez", 350, 100000.0, LocalDate.now(),true);
+
+      repository.save(book);
+      repository.save(book2);
+    }
+
+}
---
+++ Spring/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.example.obrestdatajpa.controllers;
+
+import com.example.obrestdatajpa.entities.Book;
+import com.example.obrestdatajpa.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class BookController {
+
+    @Autowired
+    BookRepository repository;
+
+    @GetMapping("/api/books")
+    public List<Book> findAll(){
+        return repository.findAll();
+    }
+
+}

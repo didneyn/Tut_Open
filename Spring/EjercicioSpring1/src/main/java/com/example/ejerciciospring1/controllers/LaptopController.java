cfd92e60a40d673350060652b834a99331ae2c21
didneyn <didney.nieto@mercadolibre.com.co>
2022-06-06 10:48:52 -0500

    :arrow_up: add exercises 7-8-9

--- Spring/EjercicioSpring1/src/main/java/com/example/ejerciciospring1/controllers/LaptopController.java
+++ Spring/EjercicioSpring1/src/main/java/com/example/ejerciciospring1/controllers/LaptopController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/laptops")
@@ -20,9 +21,61 @@ public class LaptopController {
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Laptop>> findOneById (@PathVariable Long id){
+        if(id == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        Optional<Laptop> laptop = repository.findById(id);
+        if(!laptop.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(repository.findById(id));
+    }
+
     @PostMapping
     public ResponseEntity save (@RequestBody Laptop laptop){
-        repository.save(laptop);
-        return ResponseEntity.ok().body(laptop);
+
+        if(laptop.getId()!=null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        Laptop result = repository.save(laptop);
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PutMapping
+    public ResponseEntity update (@RequestBody Laptop laptop){
+        if(laptop.getId() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if( !repository.existsById(laptop.getId()) ){
+            return ResponseEntity.notFound().build();
+        }
+        Laptop result = repository.save(laptop);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Laptop>> deleteOneById (@PathVariable Long id){
+
+        if(id == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if( !repository.existsById(id) ){
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+    @DeleteMapping
+    public ResponseEntity<Optional<Laptop>> deleteAll (){
+        repository.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ Spring/EjercicioSpring1/src/test/java/com/example/ejerciciospring1/controllers/LaptopControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.ejerciciospring1.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteOneById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}

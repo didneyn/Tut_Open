189e82fc3ed6e9f889ee879d3a3416fc6ac7b916
didneyn <didney.nieto@mercadolibre.com.co>
2022-06-08 10:45:15 -0500

    add exercises 10-11-12

---
+++ Spring/ob-spring-security/src/main/java/com/example/obspringsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.example.obspringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/hola").hasRole("USER")
+                        .antMatchers("/bootstrap").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin()
+                .and()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public HttpFirewall looseHttpFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowSemicolon(true);
+        firewall.setAllowUrlEncodedSlash(true);
+        return firewall;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        InMemoryUserDetailsManager users = new InMemoryUserDetailsManager();
+        UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER").build();
+        users.createUser(user);
+        user = User.builder().username("admin").password(passwordEncoder().encode("password")).roles("USER","ADMIN").build();
+        users.createUser(user);
+        return users;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
